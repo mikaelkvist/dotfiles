95f059fd5bc4af10646b57cafea33be244ba897b
Mikael Kvist <mkvi@kth.se>
2023-03-04 09:21:50 +0100

    Change shortcut for symbols-outline

--- config/nvim/lua/plugins/symbols-outline.lua
+++ config/nvim/lua/plugins/symbols-outline.lua
@@ -3,7 +3,7 @@ local M = { 'simrat39/symbols-outline.nvim' }
 M.config = true
 
 M.keys = {
-	{ '<leader>;', '<cmd>SymbolsOutline<cr>', desc = 'Symbols outline' }
+	{ '<leader>s', '<cmd>SymbolsOutline<cr>', desc = 'Symbols outline' }
 }
 
 return M
