debaca747d18d06f15dbae1c61f50b9414602564
Mikael Kvist <mkvi@kth.se>
2023-03-04 09:22:16 +0100

    Clean up telescope settings

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -2,18 +2,8 @@ local M = { 'nvim-telescope/telescope.nvim' }
 
 M.dependencies = {
 	'nvim-lua/plenary.nvim',
-	-- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
 	{ 'nvim-telescope/telescope-ui-select.nvim' }
 }
-
--- local fzf_opts = {
---   fuzzy = true,                    -- false will only do exact matching
---   override_generic_sorter = true,  -- override the generic sorter
---   override_file_sorter = true,     -- override the file sorter
---   case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
---                                    -- the default case_mode is "smart_case"
--- }
-
 
 M.config = function()
 	local telescope = require('telescope')
@@ -57,15 +47,10 @@ M.config = function()
 			prompt_prefix = '> ',
 		},
 		pickers = {
-			-- Manually set sorter, for some reason not picked up automatically
-			-- lsp_dynamic_workspace_symbols = {
-			-- 	sorter = telescope.extensions.fzf.native_fzf_sorter(fzf_opts)
-			-- },
 		},
 		extensions = {
 		},
 	})
-	-- telescope.load_extension('fzf')
 	telescope.load_extension('ui-select')
 end
 
