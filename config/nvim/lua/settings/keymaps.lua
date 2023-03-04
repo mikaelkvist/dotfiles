d019898ffeea39fa895d638dc4a6aa559ba72509
Mikael Kvist <mkvi@kth.se>
2023-03-04 17:01:18 +0100

    Change neovim settings

--- config/nvim/lua/settings/keymaps.lua
+++ config/nvim/lua/settings/keymaps.lua
@@ -1,5 +1,7 @@
 vim.keymap.set('v', '<', '<gv', {})
 vim.keymap.set('v', '>', '>gv', {})
 
+vim.keymap.set('n', '<leader>aq', '<cmd>bufdo bd<cr>', {})
+
 vim.keymap.set('n', 'L', '<cmd>vsplit<cr>', {})
 vim.keymap.set('n', 'J', '<cmd>split<cr>', {})
